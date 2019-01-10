35a9e39681aff32548cf801261d10869cbab20fa
Ajoy Bose <boseajoy@outlook.com>
2019-01-09 23:07:07 -0800

    simple api

---
+++ simpleweb/simpleapi.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	err := http.ListenAndServe("localhost:11111", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
